c4f3fa6a95e7ef3fd9ef382a9b1e9785baf9a72f
crespo44 <crespo_914@hotmail.com>
2025-10-16 16:45:37 +0200

    Añadida pagina de recarga arreglada

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -7,6 +7,7 @@ import { useMutation } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 import { setCredentials } from "../../slices/authSlice";
 import LoginSchema from './loginValidation';
+import Preloader from "../Preloader/Preloader";
 import Card from "../Card/Card";
 import Button from '../Buttons/Button';
 import RegisterForm from '../RegisterForm/RegisterForm';
@@ -45,27 +46,29 @@ const LoginForm = () => {
 
   return (
     <>
-      <Card>
-        <form onSubmit={handleSubmit(onSubmit)} className="login-form">
+      {mutation.isPending && <Preloader />}
+      {!mutation.isPending && (
+        <Card>
+          <form onSubmit={handleSubmit(onSubmit)} className="login-form">
 
-          <h2>Inicio de sesión</h2>
-          <div className="login_form-group">
-            <label htmlFor="username">Usuario</label>
-            <input type="text" name="username" id="username" placeholder="Introduce tu nombre de usuario"{...register('username')}/>
-            {errors.username && <p className="error">{errors.username.message}</p>}
-          </div>
-          <div className="login_form-group">
-            <label htmlFor="password">Contraseña</label>
-            <input type="password" name="password" id="password" placeholder="Introduce tu contraseña" {...register('password')}/>
-            {errors.password && <p className="error">{errors.password.message}</p>}
-          </div>
-          <div className="login-buttons">
-            <Button type="submit" text={mutation.isPending ? "Iniciando..." : "Iniciar sesión"} disabled={mutation.isPending} />
-            <Button type="button" text="Registrarse" onClick={() => setShowModal(true)} />
-          </div>
-        </form>
-      </Card>
-
+            <h2>Inicio de sesión</h2>
+            <div className="login_form-group">
+              <label htmlFor="username">Usuario</label>
+              <input type="text" name="username" id="username" placeholder="Introduce tu nombre de usuario"{...register('username')}/>
+              {errors.username && <p className="error">{errors.username.message}</p>}
+            </div>
+            <div className="login_form-group">
+              <label htmlFor="password">Contraseña</label>
+              <input type="password" name="password" id="password" placeholder="Introduce tu contraseña" {...register('password')}/>
+              {errors.password && <p className="error">{errors.password.message}</p>}
+            </div>
+            <div className="login-buttons">
+              <Button type="submit" text={mutation.isPending ? "Iniciando..." : "Iniciar sesión"} disabled={mutation.isPending} />
+              <Button type="button" text="Registrarse" onClick={() => setShowModal(true)} />
+            </div>
+          </form>
+        </Card>
+      )}
       <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
         <RegisterForm onClose={() => setShowModal(false)} />
       </Modal>
